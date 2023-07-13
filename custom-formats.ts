63f56f8f109e446a6754072a1d628e89905a8f36
Ulteh <114617278+Ulteh@users.noreply.github.com>
2023-07-14 01:01:26 +0300

    Add files via upload

---
+++ custom-formats.ts
@@ -0,0 +1,10 @@
+{
+		section: "Cyberville",
+	},
+{
+		name: "[Gen 9] OU",
+
+		mod: 'cyberville balance',
+		ruleset: ['Standard'],
+		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail'],
+	},

9f9cdce380550a315b6c2c3f1bc07d31fdc7bc24
Cowctus <dkt.prive@gmail.com>
2023-07-13 23:39:36 +0200

    Create pokedex.ts
    starter test

---
+++ data/mods/cyberville balance/pokedex.ts
@@ -0,0 +1,185 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	venusaur: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 110, spe: 80},
+		abilities: {0: "Overgrow", 1: "Natural Cure", H: "Chlorophyll"},
+	},
+	venusaurmega: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 100, def: 123, spa: 122, spd: 130, spe: 80},
+	},
+	charizard: {
+		inherit: true,
+		baseStats: {hp: 78, atk: 84, def: 78, spa: 109, spd: 85, spe: 101},
+		abilities: {0: "Blaze", 1: "Flame Body", H: "Solar Power"},
+	},
+	charizardmegax: {
+		inherit: true,
+		baseStats: {hp: 78, atk: 130, def: 111, spa: 130, spd: 85, spe: 101},
+	},
+	charizardmegay: {
+		inherit: true,
+		baseStats: {hp: 78, atk: 104, def: 78, spa: 159, spd: 115, spe: 101},
+	},
+	blastoise: {
+		inherit: true,
+		baseStats: {hp: 79, atk: 83, def: 105, spa: 85, spd: 105, spe: 78},
+		abilities: {0: "Torrent", 1: "Anger Shell", H: "Rain Dish"},
+	},
+	blastoisemega: {
+		inherit: true,
+		genderRatio: {M: 0.875, F: 0.125},
+		baseStats: {hp: 79, atk: 103, def: 125, spa: 135, spd: 115, spe: 78},
+	},
+	meganium: {
+		inherit: true,
+		baseStats: {hp: 90, atk: 82, def: 100, spa: 83, spd: 100, spe: 80},
+		abilities: {0: "Overgrow", 1: "Triage", H: "Leaf Guard"},
+	},
+	typhlosion: {
+		inherit: true,
+		baseStats: {hp: 78, atk: 84, def: 78, spa: 110, spd: 85, spe: 100},
+		abilities: {0: "Blaze", 1: "Stakeout", H: "Flash Fire"},
+	},
+	typhlosionhisui: {
+		inherit: true,
+		baseStats: {hp: 73, atk: 84, def: 78, spa: 120, spd: 85, spe: 95},
+		abilities: {0: "Blaze", 1: "Flash Fire", H: "Frisk"},
+	},
+	feraligatr: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 110, def: 100, spa: 79, spd: 83, spe: 78},
+		abilities: {0: "Torrent", 1: "Swift Swim", H: "Sheer Force"},
+	},
+	sceptile: {
+		inherit: true,
+		baseStats: {hp: 70, atk: 85, def: 65, spa: 105, spd: 85, spe: 125},
+		abilities: {0: "Overgrow", 1: "Dragon's Maw", H: "Unburden"},
+	},
+	sceptilemega: {
+		inherit: true,
+		baseStats: {hp: 70, atk: 110, def: 75, spa: 145, spd: 85, spe: 150},
+		abilities: {0: "Grassy Surge"},
+	},
+	blaziken: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 120, def: 70, spa: 115, spd: 70, spe: 80},
+		abilities: {0: "Blaze", 1: "Hustle", H: "Speed Boost"},
+	},
+	blazikenmega: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 160, def: 80, spa: 135, spd: 80, spe: 100},
+	},
+	swampert: {
+		inherit: true,
+		abilities: {0: "Torrent", 1: "Rain Dish", H: "Damp"},
+	},
+	torterra: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 109, def: 115, spa: 75, spd: 85, spe: 56},
+		abilities: {0: "Overgrow", 1: "Harvest", H: "Shell Armor"},
+	},
+	infernape: {
+		inherit: true,
+		baseStats: {hp: 76, atk: 105, def: 71, spa: 104, spd: 71, spe: 108},
+		abilities: {0: "Blaze", 1: "Scrappy", H: "Iron Fist"},
+	},
+	empoleon: {
+		inherit: true,
+		baseStats: {hp: 84, atk: 86, def: 88, spa: 111, spd: 101, spe: 65},
+		abilities: {0: "Torrent", 1: "Supreme Overlord", H: "Defiant"},
+	},
+	serperior: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 75, def: 102, spa: 75, spd: 95, spe: 113},
+		abilities: {0: "Overgrow", 1: "Shed Skin", H: "Contrary"},
+	},
+	emboar: {
+		inherit: true,
+		baseStats: {hp: 117, atk: 123, def: 65, spa: 100, spd: 65, spe: 65},
+		abilities: {0: "Blaze", 1: "Anger Point", H: "Reckless"},
+	},
+	samurott: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 107, def: 85, spa: 108, spd: 70, spe: 70},
+		abilities: {0: "Torrent", 1: "Weak Armor", H: "Shell Armor"},
+	},
+	samurotthisui: {
+		inherit: true,
+		baseStats: {hp: 90, atk: 115, def: 80, spa: 100, spd: 65, spe: 85},
+		abilities: {0: "Torrent", 1: "Infiltrator", H: "Sharpness"},
+	},
+	chesnaught: {
+		inherit: true,
+		baseStats: {hp: 88, atk: 107, def: 127, spa: 74, spd: 75, spe: 64},
+		abilities: {0: "Overgrow", 1: "Iron Barbs", H: "Bulletproof"},
+	},
+	delphox: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 69, def: 72, spa: 115, spd: 100, spe: 104},
+		abilities: {0: "Blaze", 1: "Psychic Surge", H: "Magician"},
+	},
+	greninja: {
+		inherit: true,
+		baseStats: {hp: 72, atk: 100, def: 67, spa: 103, spd: 71, spe: 122},
+		abilities: {0: "Torrent", 1: "Berserk", H: "Protean", S: "Battle Bond"},
+	},
+	greninjabond: {
+		inherit: true,
+		baseStats: {hp: 72, atk: 100, def: 67, spa: 103, spd: 71, spe: 122},
+	},
+	greninjaash: {
+		inherit: true,
+		baseStats: {hp: 72, atk: 150, def: 67, spa: 153, spd: 71, spe: 132},
+	},
+	decidueye: {
+		inherit: true,
+		baseStats: {hp: 78, atk: 107, def: 75, spa: 100, spd: 100, spe: 75},
+		abilities: {0: "Overgrow", 1: "Sniper", H: "Long Reach"},
+	},
+	decidueyehisui: {
+		inherit: true,
+		baseStats: {hp: 88, atk: 112, def: 80, spa: 95, spd: 95, spe: 65},
+		abilities: {0: "Overgrow", 1: "Unburden", H: "Scrappy"},
+	},
+	incineroar: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 115, def: 90, spa: 80, spd: 90, spe: 65},
+		abilities: {0: "Blaze", 1: "Moxie", H: "Intimidate"},
+	},
+	primarina: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 74, def: 74, spa: 126, spd: 116, spe: 60},
+		abilities: {0: "Torrent", 1: "Soul-Heart", H: "Liquid Voice"},
+	},
+	rillaboom: {
+		inherit: true,
+		baseStats: {hp: 100, atk: 125, def: 95, spa: 60, spd: 70, spe: 85},
+		abilities: {0: "Overgrow", 1: "Own Tempo", H: "Grassy Surge"},
+	},
+	cinderace: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 116, def: 75, spa: 65, spd: 80, spe: 119},
+		abilities: {0: "Blaze", 1: "Quick Feet", H: "Libero"},
+	},
+	inteleon: {
+		inherit: true,
+		baseStats: {hp: 70, atk: 90, def: 65, spa: 125, spd: 65, spe: 120},
+		abilities: {0: "Torrent", 1: "Tinted Lens", H: "Sniper"},
+	},
+	meowscarada: {
+		inherit: true,
+		baseStats: {hp: 76, atk: 110, def: 70, spa: 86, spd: 70, spe: 123},
+		abilities: {0: "Overgrow", 1: "Trace", H: "Protean"},
+	},
+	skeledirge: {
+		inherit: true,
+		baseStats: {hp: 104, atk: 75, def: 100, spa: 110, spd: 75, spe: 71},
+		abilities: {0: "Blaze", 1: "Stamina", H: "Unaware"},
+	},
+	quaquaval: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 120, def: 80, spa: 85, spd: 75, spe: 90},
+		abilities: {0: "Torrent", 1: "Dancer", H: "Moxie"},
+	},
+};

902b64d06f68dcb9f0e43c47cc81363283d1f110
Cowctus <dkt.prive@gmail.com>
2023-07-14 18:26:17 +0200

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -7,7 +7,7 @@ export const Formats: FormatList = [
 		name: "[Gen 9] Cyberville Testbranch",
 
 		mod: 'cybervillebalance',
-		ruleset: ['Standard'],
+		ruleset: ruleset: ['Team Preview', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 100', 'Default Level = 100'],
 		banlist: ['Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail'],
 	},
 ];

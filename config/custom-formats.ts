55836be793c58bbb79994aaf606a2ab69ada8544
Ulteh <114617278+Ulteh@users.noreply.github.com>
2023-07-15 00:25:00 +0300

    cyb edits

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -4,9 +4,9 @@ export const Formats: FormatList = [
 		column: 1,
 	},
 	{
-		name: "[Gen 9] Cyberville Testbranch Custom",
+		name: "[Gen 9] cyb",
 
-		mod: 'cyberbalance',
+		mod: 'gen9cyb',
 		ruleset: ['Standard'],
 		banlist: ['Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail'],
 	},

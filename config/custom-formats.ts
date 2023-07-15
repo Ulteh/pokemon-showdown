cfdd7e62d6984cc870beaca42ad89e3b5e32bf6f
MacheteMcGoo <114826431+MacheteMcGoo@users.noreply.github.com>
2023-07-15 07:36:59 -0400

    Update custom-formats.ts
    Added more rulesets
    Changed Cyb to Cyberville

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -4,10 +4,10 @@ export const Formats: FormatList = [
 		column: 1,
 	},
 	{
-		name: "[Gen 9] cyb",
+		name: "[Gen 9] Cyberville",
 
 		mod: 'gen9cyb',
-		ruleset: ['Standard'],
+		ruleset: ['Standard NatDex', 'Draft', '+Unobtainable', 'Past'],
 		banlist: ['Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail'],
 	},
 ];

8ef92a9bd9e54b80e8320613e27829facdbe6fcd
Ulteh <114617278+Ulteh@users.noreply.github.com>
2023-07-15 15:11:52 +0300

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -1,5 +1,5 @@
 export const Formats: FormatList = [	
-	{
+	/*{
 		section: "CYBERVILLE",
 		column: 1,
 	},
@@ -9,5 +9,5 @@ export const Formats: FormatList = [
 		mod: 'gen9cyb',
 		ruleset: ['Standard NatDex', 'Draft', '+Unobtainable', '+Past'],
 		banlist: ['Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass', 'Last Respects', 'Shed Tail'],
-	},
+	},*/
 ];

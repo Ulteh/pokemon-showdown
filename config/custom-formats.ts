657f88169a719e4b02b20394827acce7f4c0d36c
Cowctus <dkt.prive@gmail.com>
2023-07-15 20:33:24 +0200

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -4,10 +4,10 @@ export const Formats: FormatList = [
 		column: 1,
 	},
 	{
-		name: "[Gen 9] Cyb",
+		name: "[Gen 9] Cyberville",
 
 		mod: 'gen9cyb',
-		ruleset: ['[Gen 9] NatDex Draft', 'Draft', '+Unobtainable', '+Past'],
+		ruleset: ['[Gen 9] NatDex Draft', 'Draft', !'Obtainable', 'Obtainable Moves', 'Obtainable Misc'],
 		banlist: ['Arena Trap', 'Moody', 'Shadow Tag', 'King\'s Rock', 'Shed Tail'],
 	},
 ];

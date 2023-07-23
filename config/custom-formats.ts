339ff9f7d40906c09994199803485fb088caac73
MacheteMcGoo <114826431+MacheteMcGoo@users.noreply.github.com>
2023-07-23 11:02:52 -0400

    Update custom-formats.ts

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -7,14 +7,14 @@ export const Formats: FormatList = [
 		name: "[Gen 9] Cyberville",
 
 		mod: 'gen9cyb',
-		ruleset: ['Standard'],
+		ruleset: ['Standard', 'Min Source Gen = 9'],
 		banlist: ['Arena Trap', 'Moody', 'Shadow Tag', 'King\'s Rock', 'Shed Tail'],
 	},
 	{
 		name: "[Gen 9] Cyberville NatDexDraft",
 
 		mod: 'gen9cyb',
-		ruleset: ['Standard NatDex', '!Sleep Clause Mod', '!Evasion Clause'],
+		ruleset: ['Standard NatDex', 'Sleep Clause Mod', 'Evasion Clause', 'Draft'],
 		banlist: ['Arena Trap', 'Moody', 'Shadow Tag', 'King\'s Rock', 'Shed Tail'],
 	},
 ];
